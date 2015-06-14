e553fe476f4f64e9bc9424f6558043d369499a43
Verneuil Julien <grz.zrg@gmail.com>
2015-06-14 18:48:03 +0200

    add js solution

---
+++ 1/1.js
@@ -0,0 +1,88 @@
+ // 1:1 conversion of 1.anubis to JavaScript
+ 
+ // by Julien Verneuil, 14/06/2015
+
+"use strict";
+
+function print_usage() {
+    console.log("Usage:\n    python 1.py max n1 n2 n3 ...\n\nExample:\n    python 1.py 999 3 5");
+}
+
+function gcd(a, b) {
+    if (a < 0) { a = -a; }
+    if (b < 0) { b = -b; }
+    if (b > a) { var temp = a; a = b; b = temp; }
+    while (true) {
+        if (b == 0) { return a; }
+        a %= b;
+        if (a == 0) { return b; }
+        b %= a;
+    }
+}
+
+function lcm(a, b) {
+    return a * b / gcd(a, b);
+}
+
+function signed_lcm_list(l) {
+    if (l.length <= 0) {
+        return [];
+    }
+
+    var head = l[0],
+        tail = l.slice(1);
+
+    if (tail.length <= 0) {
+        return [head];
+    }
+
+    var p = signed_lcm_list(tail),
+        r = [head];
+    
+    for (var i = 0; i < p.length; i += 1) {
+        r.push(-lcm(head, p[i]));
+    }
+
+    r = r.concat(p);
+
+    return r;
+}
+
+function sum(n) {
+    return (n * (n + 1)) / 2;
+}
+
+var args = process.argv.slice(2),
+    args_as_int = [];
+
+for (var i = 0; i < args.length; i += 1) {
+    var n = parseInt(args[i], 10);
+
+    if (n <= 0 || isNaN(n)) {
+        console.log("Parameter \"" + s + "\" ignored, not a natural number, you should fix this.");
+    } else {
+        args_as_int.push(n);
+    }
+}
+
+if (args_as_int.length <= 0) {
+    print_usage();
+}
+
+var max = args_as_int[0],
+    numbers = args_as_int.slice(1);
+
+if (numbers.length <= 0) {
+    print_usage();
+}
+
+var slcml = signed_lcm_list(numbers),
+    result = 0;
+
+for (var i = 0; i < slcml.length; i += 1) {
+    var n = slcml[i];
+
+    result += sum(Math.floor(max / n)) * n;
+}
+
+console.log("The sum of all the multiples of " + args.join(" or ") + " below " + (max + 1) + " is " + result + ".");
