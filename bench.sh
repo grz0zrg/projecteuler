e04fde5beee33be797c5fdc0b7a04cd51437ef4b
Verneuil Julien <grz.zrg@gmail.com>
2015-08-02 16:28:37 +0200

    adapt bash script to ease compiling and benchmarking future solutions

--- bench.sh
+++ bench.sh
@@ -1,17 +1,93 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
 	echo "Usage: ./bench.sh n_times"
 	exit 1
 fi
 
-cd "$(dirname "$0")/1"
+n_times=$1
 
-echo "\n Benchmarking projecteuler.net solution 1 ..."
-echo "\n Compile Haskell solution... $(ghc 1.hs -o projecteuler_1_hs >/dev/null)done."
-echo " Compile Java solution... $(javac 1.java >/dev/null)done."
-echo "\n  \033[34mAnubis\033[0m     $(anbexec benchmark $1 . anbexec projecteuler_1 999 1 2 3 4 5 6 7 8 9 10)"
-echo "  \033[34mPython\033[0m     $(anbexec benchmark $1 . python 1.py 999 1 2 3 4 5 6 7 8 9 10)"
-echo "  \033[34mHaskell\033[0m    $(anbexec benchmark $1 . ./projecteuler_1_hs 999 1 2 3 4 5 6 7 8 9 10)"
-echo "  \033[34mJavaScript\033[0m $(anbexec benchmark $1 . node 1.js 999 1 2 3 4 5 6 7 8 9 10)"
-echo "  \033[34mJava\033[0m       $(anbexec benchmark $1 . java projecteuler_1 999 1 2 3 4 5 6 7 8 9 10)"
+compile () {
+	local cmd=""
+	local lang=""
+	
+	case "${1##*.}" in
+		"anubis" )
+			cmd="$(anubis $1 >/dev/null)"
+			lang="Anubis"
+		;;	
+
+		"hs" )
+			cmd="$(ghc $1 -o projecteuler.${1%.*}.bin >/dev/null)"
+			lang="Haskell"
+		;;
+		
+		"java" )
+			cmd="$(javac $1 >/dev/null)"
+			lang="Java"
+		;;
+	esac
+	
+	if [ -n "$lang" ]; then
+		echo "  Compile $lang solution... $cmd\033[32mdone\033[0m."
+	fi
+}
+
+benchmark () {
+	local cmd=""
+	local lang=""
+
+	case "${2##*.}" in
+		"anubis" )
+			cmd="anbexec projecteuler_$1"
+			lang="Anubis"
+		;;
+		
+		"hs" )
+			cmd="./projecteuler_$1_hs"
+			lang="Haskell"
+		;;
+		
+		"java" )
+			cmd="java projecteuler_$1"
+			lang="Java"
+		;;
+		
+		"py" )
+			cmd="python $1.py"
+			lang="Python"
+		;;
+		
+		"js" )
+			cmd="node $1.js"
+			lang="JavaScript"
+		;;
+	esac
+	
+	if [ -n "$lang" ]; then
+		echo "  \033[34;1m$lang\033[0m\n    \033[37m$(anbexec benchmark $n_times . $cmd $3)\033[0m"
+	fi
+}
+
+# take solution number as first argument and solution arguments then compile and benchmark everything
+benchmark_solution () {
+	cd $1
+	
+	echo "\n \033[32;1mBenchmarking projecteuler.net solution $1 ...\033[0m\n"
+
+	for f in $1.*; do
+		compile $f
+	done
+	
+	echo ""
+	
+	for f in $1.*; do
+		benchmark $1 $f "$2"
+	done
+	
+	cd ".."
+}
+
+cd "$(dirname $0)"
+
+benchmark_solution 1 "999 1 2 3 4 5 6 7 8 9 10"
